b1a84c399664249750c10af169721e2311142585
reece-stephenson <reeces@bbd.co.za>
2023-09-27 15:02:25 +0200

    Add express to serve static files

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 // A tip calculation app
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
+
 // Empty functions for test driven development
 function validateBillInput(billAmount) {
     return null;
